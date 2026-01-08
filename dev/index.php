1895b5473e39818a43657b73a8278fb36e9fd47f
Chaokuai07 <salmonz124518@gmail.com>
2026-01-08 21:20:25 +0700

    Refactor session handling and HTML layout
    Updated session handling and improved HTML structure.

--- dev/index.php
+++ dev/index.php
@@ -29,7 +29,7 @@ if (!isset($_SESSION["user_id"]) && !empty($_COOKIE["remember_token"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <title>Document</title>
 </head>
@@ -39,4 +39,5 @@ if (!isset($_SESSION["user_id"]) && !empty($_COOKIE["remember_token"])) {
     <a href="product-list.php">list</a>
     <a href="report-model.php">model</a>
 </body>
+
 </html>
